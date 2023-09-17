8b26190bee0ccabebaa0292f7e12bfd87b02c0a9
nzzima <nikita-krylov-2012@mail.ru>
2023-09-17 18:31:26 +0300

    Added example005 with ifElse condition

---
+++ C#_lessons/Example005_IfElseCondition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Success, it's Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);git 
+}
