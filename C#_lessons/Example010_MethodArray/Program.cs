95657fd8389a1c4704d4f0694a0462b8a59b0c7d
nzzima <nikita-krylov-2012@mail.ru>
2023-09-19 19:42:09 +0300

    Added example010 with function & array

---
+++ C#_lessons/Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 28, 365, 443, 523, 64, 7545, 443, 92};
+
+int size = array.Length;
+int find = 443;
+int index = 0;
+
+while (index < size)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
