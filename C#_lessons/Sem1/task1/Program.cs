51e7fe7ead6ddc1b46ca40cb2607030951e52d1d
nzzima <nikita-krylov-2012@mail.ru>
2023-09-23 10:27:12 +0300

    Added sem1/task1

---
+++ C#_lessons/Sem1/task1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 1. Квадрат числа
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.Write("Result: ");
+Console.WriteLine(result);
