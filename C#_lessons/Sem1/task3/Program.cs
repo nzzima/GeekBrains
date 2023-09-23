e5d55c6d65ce43edb0fc357cfa53ab79779df3a6
nzzima <nikita-krylov-2012@mail.ru>
2023-09-23 10:45:35 +0300

    Added Sem1/task3

---
+++ C#_lessons/Sem1/task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string[] week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.Write("Week day: ");
+Console.WriteLine(week[number - 1]);
