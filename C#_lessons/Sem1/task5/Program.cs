7950a381f6ba1dd3c63b00306d79ead3189b9f0e
nzzima <nikita-krylov-2012@mail.ru>
2023-09-23 10:56:29 +0300

    Added Sem1/task5

---
+++ C#_lessons/Sem1/task5/Program.cs
@@ -0,0 +1,8 @@
+﻿//Последняя цифра в трехзначном числе. 
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lastNum = number % 10;
+Console.Write("Enter last number: ");
+Console.WriteLine(lastNum);
