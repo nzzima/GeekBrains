ca6c8e77e0ca0fcfdb4f1c1ef70b172dae38e29e
nzzima <nikita-krylov-2012@mail.ru>
2023-09-23 10:36:12 +0300

    Added sem1/task2

---
+++ C#_lessons/Sem1/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2. Первое число - квадрта второго ?
+
+Console.Write("Enter number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
