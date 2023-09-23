bb02b83ccf57fc2525f0f0c23d77335bafc06870
nzzima <nikita-krylov-2012@mail.ru>
2023-09-23 10:48:13 +0300

    Added Sem1/task4

---
+++ C#_lessons/Sem1/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Обратное число
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int negNumber = -1 * number;
+
+while (negNumber <= number)
+{
+    Console.WriteLine(negNumber);
+    negNumber++;
+}
