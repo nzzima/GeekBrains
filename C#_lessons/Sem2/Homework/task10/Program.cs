35da2877509bc556f66fded6d3c38aef6ae65c9d
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 13:27:24 +0300

    Upgraded sem2/homework/task10

--- C#_lessons/Sem2/Homework/task10/Program.cs
+++ C#_lessons/Sem2/Homework/task10/Program.cs
@@ -1,9 +1,25 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Enter number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int Start()
+{
+    Console.Write("Enter number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
 
-int temp1 = number / 10;
-int temp2 = temp1 % 10;
+bool CheckNumber(int number)
+{
+    if ((number > 99) && (number < 1000))
+    {
+        Console.WriteLine("Check number: OK!");
+        return true;
+    }
+    Console.WriteLine("Check number: OUT OF RANGE!");
+    return false;
+}
 
-Console.WriteLine("Result --> " + temp2);
+int number = Start();
+if (CheckNumber(number))
+{
+    Console.WriteLine("Second rank --> " + number / 10 % 10);
+}
