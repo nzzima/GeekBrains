7dc5b7ea62f3a771efd3b9ada496c2b98f748dd4
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 13:29:05 +0300

    Upgraded sem2/homework/task13

--- C#_lessons/Sem2/Homework/task13/Program.cs
+++ C#_lessons/Sem2/Homework/task13/Program.cs
@@ -1,10 +1,17 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
 
+int Start()
+{
+    Console.Write("Enter number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
 bool Validate(int number)
 {
     if (number < 100)
     {
-        Console.WriteLine("No third number");
+        Console.WriteLine("No third rank");
         return false;
     }
     return true;
@@ -19,7 +26,6 @@ int GetThird(int number)
     return number % 10;
 }
 
-Console.Write("Enter number: ");
-int number = Convert.ToInt32(Console.ReadLine());;
+int number = Start();
 
-if (Validate(number)) Console.WriteLine("Third number --> " + GetThird(number));
+if (Validate(number)) Console.WriteLine("Third rank --> " + GetThird(number));
