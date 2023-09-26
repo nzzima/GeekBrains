bb7fe3511ec49d9d9130d425f88c7363d379cedd
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 12:25:47 +0300

    Added homework/task10

---
+++ C#_lessons/Sem2/Homework/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = new Random().Next(100,1000);
+Console.WriteLine("Entered number --> " + number);
+
+int temp1 = number / 10;
+int temp2 = temp1 % 10;
+
+Console.WriteLine("Result --> " + temp2);
