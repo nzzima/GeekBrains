e81aea46feb513bc2c18b57bb698f30723e51506
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 13:07:08 +0300

    Added homework/task15

---
+++ C#_lessons/Sem2/Homework/task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Enter week day: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if ((day == 6) || (day == 7))
+{
+    Console.WriteLine("It's weekend day!");
+}
+else
+{
+    Console.WriteLine("It's no weekend day!");
+}
