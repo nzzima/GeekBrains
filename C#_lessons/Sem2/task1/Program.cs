fb68553a60fdb059d9c2b05ea1777a5ccceb76ba
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 12:06:59 +0300

    Added sem2/task1

---
+++ C#_lessons/Sem2/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1.
+// Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10,100);
+
+Console.WriteLine(num);
+int num1 = num / 10;
+int num2 = num % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else 
+{
+    Console.WriteLine(num2);
+}
