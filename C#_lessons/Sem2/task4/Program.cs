db6ad098c895851c5d966afa657cb6fafad88695
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 12:14:26 +0300

    Added sem2/task4

---
+++ C#_lessons/Sem2/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4.
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num % 7 == 0) && (num % 23 == 0))   //  && - И     || - Или
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+     Console.WriteLine("No");
+}
