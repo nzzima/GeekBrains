8ef2ebe6d57feb9ace4038637f3754b33b1785da
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 12:11:50 +0300

    Added sem2/task3

---
+++ C#_lessons/Sem2/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3.
+// Напишите программу, которая будет принимать на вход
+// 2 числа и выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.Write("Enter number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Некратно, остаток равен " + num1 % num2);    //Канкатинация (temp1 + "text" + temp2 + "text");
+    Console.WriteLine($"Некратно, остаток равен {num1 % num2}");    //Интерполяция
+    Console.WriteLine(num1 + "\t" + num2);                          //Табуляция
+    Console.WriteLine(num1 + "\n" + num2);                          //С новой строки
+}
