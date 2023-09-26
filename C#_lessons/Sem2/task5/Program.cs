fdb3971f3d369e5401b7c6c4d8bb25f5cc0d97f6
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 12:16:18 +0300

    Added sem2/task5

---
+++ C#_lessons/Sem2/task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 5.
+// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+int num1 = new Random().Next(100,1000);
+Console.WriteLine(num1);
+
+int num2 = new Random().Next(100,1000);
+Console.WriteLine(num2);
+// int num1 = 5;
+// int num2 = 25;
+
+if(num1 == num2*num2 || num2 == num1*num1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
