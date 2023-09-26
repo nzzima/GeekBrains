22efa166cab1af4ee42757e5dae2e3c7f6fbc01e
nzzima <nikita-krylov-2012@mail.ru>
2023-09-26 12:08:38 +0300

    Added sem2/task2

---
+++ C#_lessons/Sem2/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2.
+// Напишите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
+
+Console.WriteLine(num);
+
+int num1 = num / 100;
+int num2 = num % 10;
+
+int result = num1 * 10 + num2;
+
+Console.WriteLine(result);
