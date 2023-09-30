e9997b101a1af3062e053391a6239c4a1583367a
nzzima <nikita-krylov-2012@mail.ru>
2023-09-30 11:50:57 +0300

    Added homework/task21

---
+++ C#_lessons/Sem3/Homework/task21/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 21: Напишите программу, которая принимает на
+// вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+
+using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+      double result = Math.Sqrt((pointB[0] - pointA[0])*(pointB[0] - pointA[0]) + 
+                                (pointB[1] - pointA[1])*(pointB[1] - pointA[1]) +
+                                (pointB[2] - pointA[2])*(pointB[2] - pointA[2]));
+      return result;  
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 7;
+            x2 = -5;
+            x3 = 0;
+            y1 = 1;
+            y2 = -1;
+            y3 = 9;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
