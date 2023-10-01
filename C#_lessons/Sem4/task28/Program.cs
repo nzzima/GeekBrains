a3c8030ce7ea191bfefcf865539b11666e601751
nzzima <nikita-krylov-2012@mail.ru>
2023-10-01 10:23:44 +0300

    Added sem4/task28

---
+++ C#_lessons/Sem4/task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход 
+// число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Write("Enter number: ");
+int num= Convert.ToInt32(Console.ReadLine());
+
+int GetSumNum()
+{
+    int i = 1;
+    int result = 1;
+
+    while(i<=num)
+    {
+        result= result * i;
+        i++;
+    }
+    return result;
+}
+System.Console.WriteLine(GetSumNum());
