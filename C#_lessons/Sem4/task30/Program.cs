cecad47af805fad46188a73e48c8e6a4a8de40e7
nzzima <nikita-krylov-2012@mail.ru>
2023-10-01 10:36:37 +0300

    Added sem4/task30

---
+++ C#_lessons/Sem4/task30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+
+void FillArray(int[] array)
+{
+    for(int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(0,2);
+        System.Console.Write(array[index] + " | ");
+    }
+}
+
+FillArray(array);
