06a5df4768cd54ba4dfa42de702ff25a0db79bec
nzzima <nikita-krylov-2012@mail.ru>
2023-10-01 10:14:48 +0300

    Added sem4/task24

---
+++ C#_lessons/Sem4/task24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход
+//  число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Enter number: ");
+int num= Convert.ToInt32(Console.ReadLine());
+
+int GetSunNum()
+{
+    int i =1;
+    int sum=0;
+
+    while(i<=num)
+    {
+        sum= sum+i;
+        i++;
+    }
+
+    return sum;
+}
+
+int result = GetSunNum();
+System.Console.WriteLine(result);
