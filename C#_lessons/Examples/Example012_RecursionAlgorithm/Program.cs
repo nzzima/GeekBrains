85158bc41c45048139147c5b51fe4395f118bb22
nzzima <nikita-krylov-2012@mail.ru>
2023-10-03 13:34:16 +0300

    Added example012 with matrix

---
+++ C#_lessons/Examples/Example012_RecursionAlgorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿string[,] table = new string[2, 5];
+
+// table[0,0]   table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// table[1,0]   table[1,1]  table[1,2]  table[1,3]  table[1,4]
+// ...
+
+table[1, 2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"{table[rows, columns]}");
+//     }
+// }
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[5, 5];
+
+FillArray(matrix);
+PrintArray(matrix);
