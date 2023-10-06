565459426ad8e1e223d5320d0ec516a87ad9df09
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-06 14:13:09 +0300

    Added sem5/task32

---
+++ C#_lessons/Sem5/task32/Program.cs
@@ -0,0 +1,40 @@
+﻿void FillArray(int[] array)
+{
+    for(int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for(int index = 0; index < array.Length; index++)
+    {
+        if (index == array.Length - 1)
+        {
+            System.Console.Write($"{array[index]}");
+
+        }
+        else System.Console.Write($"{array[index]}" + ", ");
+    }
+    System.Console.Write("]");
+}
+
+void ArrayNegation (int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] > 0) array[index] -= 2 * array[index];
+        else if (array[index] < 0) array[index] -= 2 * array[index];
+    }
+}
+
+int[] array = new int[5];
+FillArray(array);
+PrintArray(array);
+ArrayNegation(array);
+System.Console.WriteLine();
+;
+System.Console.Write($"Result of negation: ");
+PrintArray(array);
