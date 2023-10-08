33c7d28257a59517cb48a1819f60352307beedc2
nzzima <nikita-krylov-2012@mail.ru>
2023-10-08 13:28:13 +0300

    Added sem6/task40

---
+++ C#_lessons/Sem6/task40/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+
+System.Console.Write("Enter first: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter sacond: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter third: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+void ExistTriangle(int A, int B, int C)
+{
+    if ((A < B + C) && (B < A + C) && (C < A + B))
+    {
+        System.Console.WriteLine("Triangle exist!");
+    }
+    else
+    {
+        System.Console.WriteLine("Triangle NOT exist!");
+    }
+}
+
+ExistTriangle(A, B, C);
