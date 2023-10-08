7e29904ed9f5d036e82aabc944ddf4d2c5e0fd1b
nzzima <nikita-krylov-2012@mail.ru>
2023-10-08 13:38:34 +0300

    Added sem6/task42

---
+++ C#_lessons/Sem6/task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+System.Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string ConvertToBinary (int number)
+{
+    string result = "";
+    while(number > 0)
+    {
+        result = number % 2 + result;
+        number /= 2;
+    }
+    return result;
+}
+
+string result = ConvertToBinary(number);
+System.Console.WriteLine($"{number} converted to binary: {result}");
