04b89b26da58e0040ae100ce96d0455a3d609cab
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-09 13:20:08 +0300

    Added sem7/task48

---
+++ C#_lessons/Sem7/task48/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 48. Задайте двумерный массив размера m*n, каждый элемент в массиве находится по формуле A = m + n. Вывести на
+// экран.
+
+int EnterValue(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = i + j;
+        }
+    }
+}
+
+int m = EnterValue("Enter count of str: ");
+int n = EnterValue("Enter count of stolb: ");
+
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
