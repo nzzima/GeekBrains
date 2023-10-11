53c69365034318c8837519a3090feb011ac3b33a
nzzima <nikita-krylov-2012@mail.ru>
2023-10-11 19:35:38 +0300

    Added examples/example015 with tree operation

---
+++ C#_lessons/Examples/Example015_OperationTree/Program.cs
@@ -0,0 +1,19 @@
+﻿// Построение дерева операции
+
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+//                 0    1    2    3     4    5    6    7    8    9    10   11
+
+void inOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) inOrderTraversal(left);
+        System.Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) inOrderTraversal(right);
+    }
+}
+
+inOrderTraversal();
