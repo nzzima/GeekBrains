876d96d896ff7507d700cee437c72213c24912c7
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-11 16:36:14 +0300

    Added example/example013 with recursive directories and files view

---
+++ C#_lessons/Examples/Example013_RecursiveDirectory/Program.cs
@@ -0,0 +1,23 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    //string path = "C:/Users/nzzima/Desktop/GitHub/GeekBrains/C#_lessons/Examples/Example013_RecursiveDirectory";
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"C:/Users/nzzima/Desktop/GitHub/GeekBrains/C#_lessons/Examples/Example013_RecursiveDirectory";
+CatalogInfo(path);
