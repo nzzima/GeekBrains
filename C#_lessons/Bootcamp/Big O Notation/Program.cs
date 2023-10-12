bcdb9c28daef773c3d27e871f625b9f15e963a10
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-12 14:01:48 +0300

    Added second lesson of bootcamp

---
+++ C#_lessons/Bootcamp/Big O Notation/Program.cs
@@ -0,0 +1,31 @@
+﻿// int n = 5;
+// int[] array = new int[n];
+
+// for (int i = 0; i < n; i++)
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+// int sum = 0;
+// for (int i = 0; i < n; i++)
+//     sum += array[i];
+// System.Console.WriteLine(sum);
+
+//O(n)--------------------------------------------------------------------
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for(int i = 1; i <= n; i++)
+{
+    for(int j = 1; j <= n; j++)
+    {
+        if(i + j <= n)
+        {
+            matrix[i, j] = i * j;
+            matrix[j, i] = i * j;
+        }
+        // System.Console.Write(i * j);
+        // System.Console.Write("\t");
+    }
+    System.Console.WriteLine();
+}
+// O(n^2)
