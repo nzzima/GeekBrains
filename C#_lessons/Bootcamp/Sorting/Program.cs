2c43b389f838e4fdd3747f79850fbe33bccf3a81
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-13 10:04:01 +0300

    Added new lesson to BootCamp: Sorting class with selection sorting

---
+++ C#_lessons/Bootcamp/Sorting/Program.cs
@@ -0,0 +1,6 @@
+﻿using static Sorting;
+
+int[] array = new int[] {9, 6, 0, 5, 7, 3, 1, 2};
+System.Console.WriteLine(string.Join(' ', array));
+array.SortSelection();
+System.Console.WriteLine(string.Join(' ', array));
---
+++ C#_lessons/Bootcamp/Sorting/Sorting.cs
@@ -0,0 +1,27 @@
+/// <summary>
+/// Класс сортировок массива.
+/// </summary>
+public static class Sorting
+{
+    /// <summary>
+    /// Сортировка методов выбора
+    /// </summary>
+    /// <param name="collection">Исходный массив</param>
+    /// <returns>Отсортированный массив</returns>
+    public static int[] SortSelection(this int[] collection)
+    {
+        int size = collection.Length;
+        for(int i = 0; i < size - 1; i++)
+        {
+            int pos = i;
+            for(int j = i + 1; j < size; j++)
+            {
+                if (collection[j] < collection[pos]) pos = j;
+            }
+            int temp = collection[i];
+            collection[i] = collection[pos];
+            collection[pos] = temp;
+        }
+        return collection;
+    }
+}
