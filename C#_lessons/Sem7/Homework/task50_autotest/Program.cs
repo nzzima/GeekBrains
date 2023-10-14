477b3639087e04894a15bea88d4587dc3944235e
nzzima <nikita-krylov-2012@mail.ru>
2023-10-14 13:26:58 +0300

    Added sem7/homework/task50_autotest

---
+++ C#_lessons/Sem7/Homework/task50_autotest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class Answer {
+    public static void PrintArray (int [,] matrix)
+    {
+        for (int i=0; i<matrix.GetLength(0); i++)
+        {
+            for (int j=0; j<matrix.GetLength(1);j++)
+            {
+                Console.Write($"{matrix[i,j]}"+"\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+        int[,] newMatrix = new int[n, m];
+        int currentValue = 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                newMatrix[i, j] = currentValue;
+                currentValue += k;
+            }
+        }
+        return newMatrix;
+    }
+
+      public static int[] FindNumberByPosition(int[,] matrix, int rowPosition, int columnPosition)
+    {
+        int[] number = { 0, -1 };
+        if (rowPosition >= 0 && rowPosition < matrix.GetLength(0) && columnPosition >= 0 && columnPosition < matrix.GetLength(1))
+        {
+            number[1] = 0;
+            number[0] = matrix[rowPosition, columnPosition];
+        }
+        return number;
+    }
+  public static void PrintCheckIfError (int[] results, int X, int Y)
+    {
+        if (results[1]==-1){Console.WriteLine("There is no such index");}
+        else {Console.WriteLine($"The number in [{X}, {Y}] is {results[0]}");
+        }
+    }
+
+    static public void Main(string[] args) {
+        int n, m, k, x, y;
+
+        if (args.Length >= 5) 
+        {
+            n = int.Parse(args[0]);
+            m = int.Parse(args[1]);
+            k = int.Parse(args[2]);
+            x = int.Parse(args[3]);
+            y = int.Parse(args[4]);
+        } else {
+            n = 3;
+            m = 4;
+            k = 2;
+            x = 2;
+            y = 3;
+        }
+
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintCheckIfError(FindNumberByPosition(result, x, y), x, y);
+    }
+}
