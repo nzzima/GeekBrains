5f9c646c17368e4631247b1a9b1fa81bb62a53f0
nzzima <nikita-krylov-2012@mail.ru>
2023-10-14 14:36:26 +0300

    Added sem7/homework/task52_autotest

---
+++ C#_lessons/Sem7/Homework/task52_autotest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Answer {
+    public static void PrintArray (int [,] matrix)
+    {
+        for (int i=0; i<matrix.GetLength(0); i++)
+        {
+            for (int j=0; j<matrix.GetLength(1);j++)
+            {
+                Console.Write($"{matrix[i,j]}"+"\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+        int[,] newMatrix = new int[n,m];
+        int currentValue = 1;
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                newMatrix[i,j] = currentValue;
+                currentValue += k;
+            }
+        }
+        return newMatrix;
+    }
+
+    static void PrintListAvr (double [] list)
+    {
+        Console.WriteLine("The averages in columns are:");
+        foreach (double el in list)
+        {
+            Console.Write($"{el:f2}"+"\t");
+        }
+        Console.WriteLine();
+    }
+
+    static double [] FindAverageInColumns (int [,] matrix)
+    { 
+        double runningSum=0;
+        double [] columns = new double [matrix.GetLength(1)];
+        for (int j=0; j<matrix.GetLength(1);j++)
+        {
+            runningSum=0;
+            for (int i=0; i<matrix.GetLength(0);i++)
+            {
+                runningSum=runningSum+matrix[i,j];
+            }
+            columns[j]=runningSum/matrix.GetLength(0);
+        }
+        return columns;
+    }
+
+
+    static public void Main(string[] args) {
+        int n, m, k;
+
+        if (args.Length >= 3) {
+           n = int.Parse(args[0]);
+           m = int.Parse(args[1]);
+           k = int.Parse(args[2]);
+        } else {
+           n = 3;
+           m = 4;
+           k = 2;
+        }
+
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintListAvr(FindAverageInColumns(result));
+    }
+}
