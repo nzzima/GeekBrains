d413fa167acfbd2afd5761767809b4bd0edb1f26
Nikita Krylov <nzzima@MacBook-Pro-Nikita.local>
2023-10-21 17:52:55 +0300

    Added sem9/task67

---
+++ C#_lessons/Sem9/task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: напишите программу, которая будет принимать на вход
+// число и возвращать сумму его цифр.
+
+System.Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+int GetSum(int number)
+{
+    sum += number % 10;
+    if(number == 0) return sum;
+    return GetSum(number / 10);
+}
+
+sum = GetSum(number);
+System.Console.WriteLine($"Result sum = {sum}");
