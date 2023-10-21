8e357e558cfbbc97e8ab0d4ca1ad140b66e3f878
Nikita Krylov <nzzima@MacBook-Pro-Nikita.local>
2023-10-21 16:21:27 +0300

    Added sem9/task63

---
+++ C#_lessons/Sem9/task63/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 63. Задайте значение N. Напишите программу
+// которая выведет все натуральные числа от 1 до N.
+
+System.Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return start + " " + PrintNumbers(start+1, end);
+}
+
+System.Console.WriteLine(PrintNumbers(1, number));
