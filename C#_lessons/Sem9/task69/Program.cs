0294d0024c14fa4fa5da35aee0d4060e12339a52
Nikita Krylov <nzzima@MacBook-Pro-Nikita.local>
2023-10-21 18:26:22 +0300

    Added sem8/task69

---
+++ C#_lessons/Sem9/task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 69: напишите программу, которая на вход принимает 
+// 2 числа A и В и возводит число А в целую степень В.
+
+System.Console.Write("Enter number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter number B : ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+int InDegree(int number, int degree)
+{
+    result *= number;
+
+    if(degree == 0) return 1;
+    else if (degree == 1) return number;
+    return number * InDegree(number, degree - 1);
+}
+
+System.Console.WriteLine(InDegree(numberA, numberB));
