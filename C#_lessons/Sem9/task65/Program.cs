8872d5629f3eefe6a1a95ca68086e24b0cf3ee12
Nikita Krylov <nzzima@MacBook-Pro-Nikita.local>
2023-10-21 16:41:17 +0300

    Added sem9/task65

---
+++ C#_lessons/Sem9/task65/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 65. Задайте значения M и N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от M до N.
+
+System.Console.Write("Enter first number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter second number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int M, int N)
+{
+    System.Console.WriteLine(M);
+    
+    if(M == N) return;
+    else PrintNumbers(M+1, N);
+}
+
+PrintNumbers(M, N);
