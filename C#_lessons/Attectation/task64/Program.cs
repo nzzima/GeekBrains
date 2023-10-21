cb49cd54bbeae1ba7e6ca6c90bc5ec06d9a34d04
Nikita Krylov <nzzima@MacBook-Pro-Nikita.local>
2023-10-21 18:38:05 +0300

    Added attectation/task64

---
+++ C#_lessons/Attectation/task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+System.Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return start + " " + PrintNumbers(start - 1, end);
+}
+
+System.Console.WriteLine(PrintNumbers(number, 1));
